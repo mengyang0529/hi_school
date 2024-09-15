905ad72b683ecebe30fd1403ade5334c5d41df3c
Yang Meng <mengyang0529@gmail.com>
2024-09-16 00:46:01 +0900

    fix import_schools

--- schools_maps/management/commands/import_schools.py
+++ schools_maps/management/commands/import_schools.py
@@ -1,33 +1,36 @@
 # schools_maps/management/commands/import_schools.py
-import os
+
 import json
 import numpy as np
 from django.core.management.base import BaseCommand
 from schools_maps.models import School
-from django.db import models
+
 
 class Command(BaseCommand):
     help = 'Load schools data from JSON file'
-    FILE_PATH = './data_analysis/geo_schools.json'  # Constant for file path
-    def handle(self, *args, **kwargs):
+    
+    FILE_PATH: str = './data_analysis/geo_schools.json'  # Constant for file path
+
+    def handle(self, *args, **kwargs) -> None:
         try:
             self.load_schools_data()
             self.stdout.write(self.style.SUCCESS('Successfully loaded data'))
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR(f'File not found: {self.FILE_PATH}'))
         except json.JSONDecodeError:
-            self.stdout.write(self.style.ERROR('Error decoding JSON'))
+            self.stdout.write(self.style.ERROR('Error decoding JSON. Please check the file format.'))
         except Exception as e:
-            self.stdout.write(self.style.ERROR(f'An error occurred: {str(e)}'))
+            self.stdout.write(self.style.ERROR(f'An unexpected error occurred: {str(e)}'))
 
-    def load_schools_data(self):
+    def load_schools_data(self) -> None:
         with open(self.FILE_PATH, 'r') as file:
             data = json.load(file)
-            schools_to_create = self.create_school_instances(data)
-            School.objects.bulk_create(schools_to_create)
+            school_instances = self.create_school_instances(data)
+            School.objects.bulk_create(school_instances)
 
-    def create_school_instances(self, data):
+    def create_school_instances(self, data: list) -> list:
         schools = []
+        
         for item in data:
             properties = item['properties']
             location = item['geometry']
@@ -44,4 +47,5 @@ class Command(BaseCommand):
                 url=properties['official_website']
             )
             schools.append(school)
+        
         return schools
